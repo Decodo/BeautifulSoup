43081d407ed05b8466d58805e0b582fd7f192c71
Žilvinas <159907476+sakanamk@users.noreply.github.com>
2025-04-25 10:23:11 +0300

    Update beautifulsoup.py

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ip.smartproxy.com/json'
+url = 'https://ip.decodo.com/json'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 
-proxies = {'http': 'http://username:password@gate.smartproxy.com:7000', # Your username, password for proxy authentication, and desired endpoint within punctuation marks ('')
-          'https': 'http://username:password@gate.smartproxy.com:7000'} 
+proxies = {'http': 'http://username:password@gate.decodo.com:7000', # Your username, password for proxy authentication, and desired endpoint within punctuation marks ('')
+          'https': 'http://username:password@gate.decodo.com:7000'} 
 
 soup = BeautifulSoup(requests.get(url, headers=headers, proxies=proxies).text, 'lxml')
 
